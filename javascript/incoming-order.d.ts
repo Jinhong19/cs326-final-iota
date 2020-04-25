a49a48b7496db2c6c93f98d11cabf2bba235c92e
DonnyWu <dongweiwu@umass.edu>
2020-04-24 22:34:35 -0400

    tsc conversion

--- javascript/incoming-order.d.ts
+++ javascript/incoming-order.d.ts
@@ -1,3 +1,5 @@
 declare const url = "http://localhost:8080/orders";
 declare function readOrders(): Promise<void>;
 declare function renderOrders(orders: any): void;
+declare function updateOrders(): Promise<void>;
+declare function postData(url: any, data: any): Promise<Response>;
